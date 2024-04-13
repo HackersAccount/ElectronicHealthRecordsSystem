4322226db52948c6745efa1a6cae4f1319bb2c2d
Munashe Njanji <munashenjanji45@gmail.com>
2024-04-13 20:21:09 +0200

    Added the settings for database The is use of dotenv to load enviromental variable in .env file which has database connection settings for now.

--- ziggers/settings.py
+++ ziggers/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,10 +75,16 @@ WSGI_APPLICATION = 'ziggers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+load_dotenv()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'tasks'),
+        'USER': os.getenv('DB_USER', 'anonymoususerontheplay6367'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'canolnlyguessmaybe123&*%'),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
