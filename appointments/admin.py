3f835e44af844ee61365473ac63cf3a8443a4141
Munashe Njanji <munashenjanji45@gmail.com>
2024-05-02 07:12:10 +0200

    the adminstration for the appointments application

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from .models import (
+    Appointment,
+    Participant,
+    Actor,
+    Slot,
+    Reason,
+    ServiceCategory,
+    ServiceType,
+    Specialty,
+)
 
-# Register your models here.
+
+class ParticipantInline(admin.TabularInline):
+    model = Participant
+    extra = 1
+
+
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ("name",)  # Ensure this references valid fields
+    search_fields = ("name",)
+
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "start",
+        "status",
+        "appointment_type",
+        "service_category",
+        "service_type",
+        "specialty",
+    )
+    search_fields = (
+        "start",
+        "status",
+        "appointment_type",
+        "service_category__name",
+        "service_type__name",
+        "specialty__name",
+    )
+    inlines = [ParticipantInline]
+    list_filter = (
+        "status",
+        "appointment_type",
+        "service_category",
+        "service_type",
+        "specialty",
+    )
+
+
+admin.site.register(ServiceCategory)
+admin.site.register(ServiceType)
+admin.site.register(Specialty)
+admin.site.register(Slot)
+admin.site.register(Reason)
+admin.site.register(Actor, ActorAdmin)
+admin.site.register(Appointment, AppointmentAdmin)
